f8d48bd235cf9b6cac8ec7d86b6eab6fff16d22f
Patrick van Broeckhuijsen <github@patb.ro>
2022-07-27 22:45:56 +0200

    Add Jan-Felix, add newsletter functionality, minor improvements to error handling

--- index.php
+++ index.php
@@ -1,18 +1,33 @@
 <?php
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+
+	require 'PHPMailer/src/Exception.php';
+	require 'PHPMailer/src/PHPMailer.php';
 	require('passwords.php');
 
 	$passwords = fetch_passwords();
 
 	$persons = array_keys($passwords);
 
-	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['newsletter'])) {
+		if (!empty($_POST['name']) && !empty($_POST['email'])) {
+			$myfile = fopen("newsletter.txt", "a") or die("Unable to open newsletter database!");
+            $txt = $_POST['email'] .",". $_POST['name'] ."\n";
+            fwrite($myfile, $txt);
+            fclose($myfile);
+			die("Subscribed!");
+		}
+	}
+
+	if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['form'])) {
       	if (isset($_POST['person'])) {
         	$update_person = $_POST['person'];  
         }
       	
       	if (!empty($_POST['password'])) {
           	if ($_POST['password'] != $passwords[$update_person]) {
-                die("Password is incorrect");
+                die("COMPUTER SAYS NO! Password is incorrect");
             }
 
             $date_array = explode('-', $_POST['date']);
@@ -29,14 +44,45 @@
             $day = date('d', strtotime($_POST['date']));
 
             if (!is_numeric($_POST['value'])) {
-                die("Entered value is invalid");
+                die("Entered value is not numeric");
+            }
+
+            if ($_POST['value'] < 0 || $_POST['value'] > 100) {
+                die("Impossibru! Value should be between 0 and 100");
+            }
+
+            // Require user to input the day they started dating, if no values were added in the past
+            if (filesize($update_person .".txt") == 0 && $_POST['value'] != 0) {
+                die("Not too fast, son! There is no initial start date known yet. You have to add a value of 0 (zero) on the day you started dating first. Then you can add any value you like");
             }
 
             $myfile = fopen($update_person .".txt", "a") or die("Unable to open database!");
             $txt = "[Date.UTC(". $year .",(". $month ."-1),". $day ."), ". $_POST['value'] ."],\n";
             fwrite($myfile, $txt);
             fclose($myfile);
-        }
+
+			$mail = new PHPMailer();
+			$mail->setFrom('lukassinus@vanbroeckhuijsenvof.nl', 'Lukas Sinus');
+
+			$myfile = fopen("newsletter.txt", "r") or die("Unable to open newsletter database");
+			// Output one line until end-of-file
+			while(!feof($myfile)) {
+  				$pieces = explode(",", fgets($myfile));
+				$mail->addAddress($pieces[0], $pieces[1]);
+			}
+			fclose($myfile);
+
+			$mail->CharSet = PHPMailer::CHARSET_UTF8;
+			$mail->Subject = 'LUKAS SINUS ALARMMMMM!!';
+			$mail->Body = "Iemand heeft een nieuwe waarde toegevoegd op www.lukassinus.xyz \n\nGa snel naar de website om te checken hoe dit liefdesverhaal afgelopen is!";
+
+			//send the message, check for errors
+			if (!$mail->send()) {
+				die("Mailer error: ". $mail->ErrorInfo);
+			} else {
+				die("Updated! Newsletter has been sent");
+			}
+		}
 	}
 
 	$series = array();
@@ -102,6 +148,15 @@
 		<main class="px-3">
             <p class="lead">De laatste status van <?php foreach($persons as $person) { if (!empty($passwords[$person])) { if ($persons[sizeof($persons)-1] == $person) { echo 'en '. ucwords($person); } else { echo ucwords($person) .', '; } } } ?> hun sinus a.k.a. liefdesleven wordt nauwlettend bijgehouden. Zodra er nieuws is, zullen ze een nieuwe waarde toevoegen. Zal de grafiek de vorm van een sinus aanhouden?</p>
 			<p class="text-muted">Vroeger stond hier alleen de sinus van Lukas, vandaar lukassinus.xyz :)</p>
+
+			<p>Meld je hieronder aan voor de nieuwsbrief.<br /><u>Vul je echte naam in</u> om te voorkomen dat de email in de spam terecht komt.</p>
+
+			<form method="post" action="" id="newsletter">
+				<input type="text" name="name" placeholder="Voornaam Achternaam" /><br />
+				<input type="email" name="email" placeholder="lukas@sinus.nl" /><br />
+				<button type="submit" name="newsletter">Aanmelden</button><br />
+			</form>
+
 			<div id="container" style="width:100%; height:500px;"></div>
 		</main>
 
@@ -124,7 +179,7 @@
 					<input type="password" name="password" placeholder="Password" /><br />
 					<input type="text" name="date" placeholder="d-m-Y" /> 
 					<input type="number" name="value" placeholder="Value" /><br /><br />
-					<button type="submit" name="btnSubmit">Submit</button>
+					<button type="submit" name="form">Submit</button>
 				</form>
 			</p>
 		</footer>
@@ -195,6 +250,10 @@
 					name: 'Scharrel (Stef)',
 					data: [<?php echo $series["stef"]; ?>],
 					color: '#00ff00'
+				}, {
+					name: 'Hanna (Jan-Felix)',
+					data: [<?php echo $series["jan-Felix"]; ?>],
+					color: '#7b32a8'
 				}]
 			});
 		});
